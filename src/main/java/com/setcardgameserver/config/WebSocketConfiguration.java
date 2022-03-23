8a68fd34c395b0a9483c2053ef7a7b9ae678dcbe
Dani <c0r31n@inf.elte.hu>
2022-03-23 16:37:11 +0100

    added new endpoint

--- src/main/java/com/setcardgameserver/config/WebSocketConfiguration.java
+++ src/main/java/com/setcardgameserver/config/WebSocketConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/gameplay");
         registry.addEndpoint("/gameplay").setAllowedOrigins("*").withSockJS();
 
     }
